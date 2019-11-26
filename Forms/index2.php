d9c8765e52a9cf1690d0262532b994beb3053524
Markko Poljakov <markkopoljakov@Markkos-MacBook-Pro.local>
2019-11-26 14:08:18 +0200

    Ülesanne 3 ja Muu

--- Forms/index2.php
+++ Forms/index2.php
@@ -1,5 +1,3 @@
-<?php
-?>
 
 <!doctype html>
 <html>
@@ -9,7 +7,7 @@
 </head>
 <body>
 <h1>Kujundid</h1>
-<form action="Kujundid.php" method="get">
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="get">
     <h1>Kera</h1>
     Raadius <input type="text" name="t1"><br>
     <h1>Koonus</h1>
@@ -22,3 +20,20 @@
 </form>
 </body>
 </html>
+
+<?php
+$kera = $_GET['t1'];
+$kõrgus = $_GET['t2'];
+$raadius = $_GET['t3'];
+$skorgus = $_GET['t4'];
+$sraadius = $_GET['t5'];
+echo 'Kera Raadius'.'Kera ruumala:'.(4/3*3.14*($kera*$kera*$kera)).'<br>';
+echo 'Koonuse Raadius: '.$raadius.' Ja '.' Koonuse Kõrgus: '.$kõrgus. ' Koonuse ruumala: '.(1/3*3.14*pow($raadius, 2)*$kõrgus).'<br>';
+echo 'Silindri Raadius: '.$sraadius.' Ja '.' Silindri Kõrgus: '.$skorgus. ' Silindri ruumala: '.(3.14*pow($sraadius, 2)*$skorgus).'<br>';
+if (filter_var($kera, FILTER_VALIDATE_NUMBER)) {
+    echo "Kera raadius'$kera' on õige.\n";
+}
+else {
+    echo "Kera raadius $kera on vale.\n";
+}
+?>
