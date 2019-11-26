799d87591f4b5ab2748f6e254338025ee175c2d8
Markko Poljakov <markkopoljakov@Markkos-MacBook-Pro.local>
2019-11-26 12:12:01 +0200

    Ülesanne 5 Metshein.ee

--- Arrays.php
+++ Arrays.php
@@ -46,22 +46,5 @@ foreach ($raamatud as $raamat=>$andmed){
 }
 echo "Raamatuid kokku:";
 echo count($raamatud);
-?>
-    <br>
-    <br>
-<?php
-//mitmemõõtmelised massiivid
-$riigid = array(
-    'Eesti'=>array('pealinn'=>'Tallinn','rahvaarv'=>1340000),
-    'Austria'=>array('pealinn'=>'Viin','rahvaarv'=>8356700),
-    'Belgia'=>array('pealinn'=>'Brüssel','rahvaarv'=>10827500)
-);
 
-foreach ($riigid as $riik=>$andmed){
-    echo "$riik: ";
-    foreach($andmed as $detailid){
-        echo "$detailid ";
-    }
-    echo "<br>";
-}
 ?>
